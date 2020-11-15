bdbed79a14355167cc9bb53c27feeaebccbbcdd5
Jean Courtot <jean.courtot@gmail.com>
2020-11-15 19:07:42 +0100

    gestion de l'affichage des tricks: seulement 5 au chargement de la page accueil

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,20 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Trick;
+use App\Repository\TrickRepository;
+use Twig\Environment;
+
 
 class HomepageController extends AbstractController
 {
     /**
+     * @var Environment
+     */
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function index(TrickRepository $trickRepository)
     {
-        $repo = $this->getDoctrine()->getRepository(Trick::class);
-        $tricks = $repo->findAll();
-
-        return $this->render('homepage/index.html.twig', [
-            'tricks' => $tricks
-        ]);
+        return new Response($this->twig->render("homepage/index.html.twig", [
+            'tricks' => $trickRepository->getTricks(1, 5),
+        ]));
     }
 }
