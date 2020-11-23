e96545482642ebae3a06f6d653166e0d4594a1c0
Jean Courtot <jean.courtot@gmail.com>
2020-11-23 09:35:57 +0100

    ajout contrainte email unique

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,9 +6,13 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(
+ *  fields= {"email"},
+ *  message= "L'email est déjà utilisé.")
  */
 class User implements UserInterface
 {
@@ -21,6 +25,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
      */
     private $email;
 
