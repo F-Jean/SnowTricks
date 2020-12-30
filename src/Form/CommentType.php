4514ec882604326f6125abe1696f3d087bc8e721
Jean Courtot <jean.courtot@gmail.com>
2020-12-30 13:02:06 +0100

    fix affichage message erreur forms

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -6,8 +6,6 @@ use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CommentType extends AbstractType
@@ -16,7 +14,7 @@ class CommentType extends AbstractType
     {
         $builder
             ->add('content',  TextareaType::class, array (
-                'label' => 'Votre commentaire',
+                'label' => false,
             ))
         ;
     }
