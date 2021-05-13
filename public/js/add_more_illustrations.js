c561101e1a0fea2fe7e02da5730b2bb2c88b5caf
Jean Courtot <jean.courtot@gmail.com>
2021-05-13 19:01:09 +0200

    show image on select

--- public/js/add_more_illustrations.js
+++ public/js/add_more_illustrations.js
@@ -1,8 +1,9 @@
+/* ADD & DELETE BUTTONS */
 const newItem = (e) => {
     const collectionHolder = document.querySelector(e.currentTarget.dataset.collection);
 
     const item = document.createElement("div");
-    item.classList.add("col-4");
+    item.classList.add("media");
     item.innerHTML = collectionHolder
         .dataset
         .prototype
@@ -19,8 +20,28 @@ const newItem = (e) => {
 
 document
     .querySelectorAll('.btn-remove')
-    .forEach(btn => btn.addEventListener("click", (e) => e.currentTarget.closest(".col-4").remove()));
+    .forEach(btn => btn.addEventListener("click", (e) => e.currentTarget.closest(".media").remove()));
 
 document
     .querySelectorAll('.btn-new')
     .forEach(btn => btn.addEventListener("click", newItem));
+
+/* SHOW IMAGE ON SELECT */
+
+document
+    .querySelector("input").onchange = function() {previewFile()};
+
+function previewFile() {
+    var preview = document.querySelector('img');
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+        preview.src = reader.result;
+    }, 
+    false);
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
