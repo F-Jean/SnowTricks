ae11cd7a740eef5a6dec6a10e6dd20fda36bb457
F-Jean <jean.courtot@gmail.com>
2021-06-21 11:56:23 +0200

    fix issues show image on select

--- public/js/add_more_illustrations.js
+++ public/js/add_more_illustrations.js
@@ -1,39 +1,6 @@
-/* ADD & DELETE BUTTONS */
-const newItem = (e) => {
-    const collectionHolder = document.querySelector(e.currentTarget.dataset.collection);
-
-    const item = document.createElement("div");
-    item.classList.add("media");
-    item.innerHTML = collectionHolder
-        .dataset
-        .prototype
-        .replace(
-            /__name__/g, 
-            collectionHolder.dataset.index
-        );
-    item.querySelector(".btn-remove").addEventListener("click", () => item.remove());
-
-    collectionHolder.appendChild(item);
-
-    collectionHolder.dataset.index++;
-};
-
-document
-    .querySelectorAll('.btn-remove')
-    .forEach(btn => btn.addEventListener("click", (e) => e.currentTarget.closest(".media").remove()));
-
-document
-    .querySelectorAll('.btn-new')
-    .forEach(btn => btn.addEventListener("click", newItem));
-
-/* SHOW IMAGE ON SELECT */
-
-document
-    .querySelector("input").onchange = function() {previewFile()};
-
-function previewFile() {
-    var preview = document.querySelector('img');
-    var file = document.querySelector('input[type=file]').files[0];
+function previewFile(item) {
+    var preview = item.querySelector('img');
+    var file = item.querySelector('input[type=file]').files[0];
     var reader = new FileReader();
 
     reader.addEventListener("load", function () {
@@ -45,3 +12,30 @@ function previewFile() {
         reader.readAsDataURL(file);
     }
 }
+
+/* ADD & DELETE BUTTONS */
+const newItem = (e) => {
+    // pick up the collections "#illustrations & #videos"
+    const collectionHolder = document.querySelector(e.currentTarget.dataset.collection);
+    // create a div class media in JS
+    const item = document.createElement("div");
+    item.classList.add("media");
+    // replace the prototype /__name__/ by our index
+    item.innerHTML = collectionHolder
+        .dataset
+        .prototype
+        .replace(
+            /__name__/g, 
+            collectionHolder.dataset.index
+        );
+    // remove the whole div when clicking the remove btn
+    item.querySelector(".btn-remove").addEventListener("click", () => item.remove());
+    collectionHolder.appendChild(item);
+    item.querySelector("input").addEventListener("change", function() {previewFile(item)});
+    // increase index
+    collectionHolder.dataset.index++;
+};
+
+document
+    .querySelectorAll('.btn-new')
+    .forEach(btn => btn.addEventListener("click", newItem));
