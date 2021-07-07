f4fdb789471a5b0b5333ae428214559d86fc323f
F-Jean <jean.courtot@gmail.com>
2021-07-07 12:22:15 +0200

    add userChecker for login

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user): void
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user): void
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if (!$user->getEnabled()) {
+            throw new CustomUserMessageAccountStatusException('Compte inactif, consulter vos e-mails pour activer votre compte. ');
+        }
+    }
+}
